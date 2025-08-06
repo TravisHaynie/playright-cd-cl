15dec6251a2336ca8ac1f499fcda8e917b04f964
unknown <travihay@gmail.com>
2025-08-06 13:50:27 -0500

    Intentionally break test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,5 +2,5 @@ const { test, expect } = require('@playwright/test');
 
 test('homepage has correct title', async ({ page }) => {
   await page.goto('https://example.com');
-  await expect(page).toHaveTitle('Example Domain');
+  await expect(page).toHaveTitle('this test will fail');
 });
